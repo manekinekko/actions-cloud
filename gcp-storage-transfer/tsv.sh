0ff06424672cae6d2f658e301922df0829653c11
Wassim CHEGHAM <github@wassimchegham.com>
2017-08-08 00:35:12 +0200

    fix: update archive URL

--- gcp-storage-transfer/tsv.sh
+++ gcp-storage-transfer/tsv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-archiveIn='https://github.com/actions-on-google-builder/actions-on-google-project-template/archive/master.zip';
+archiveInGCP='https://storage.googleapis.com/project-2966895523221308821.appspot.com/actions-on-google-project-template-master.zip';
 filename='actions-on-google-project-template'
 
-curl --silent --output $filename.zip $archiveIn
+curl --silent --output $filename.zip $archiveInGCP
 size=$(ls -l $filename.zip | awk '{print $5}')
 md5=$(openssl md5 -binary $filename.zip | openssl enc -base64)
 
 echo "TsvHttpData-1.0" > $filename.tsv
-echo -e "$archiveIn\t$size\t$md5\n" >> $filename.tsv
+echo -e "$archiveInGCP\t$size\t$md5\n" >> $filename.tsv
 
 rm -fr $filename.zip
