3a420dd0fcb383684642a5dbd4dfbfd629d582b4
Wassim CHEGHAM <github@wassimchegham.com>
2017-08-07 21:48:45 +0200

    chore: update tsv file

--- gcp-storage-transfer/tsv.sh
+++ gcp-storage-transfer/tsv.sh
@@ -8,6 +8,6 @@ size=$(ls -l $filename.zip | awk '{print $5}')
 md5=$(openssl md5 -binary $filename.zip | openssl enc -base64)
 
 echo "TsvHttpData-1.0" > $filename.tsv
-echo "$archiveIn  $size $md5" >> $filename.tsv
+echo -e "$archiveIn\t$size\t$md5\n" >> $filename.tsv
 
 rm -fr $filename.zip
