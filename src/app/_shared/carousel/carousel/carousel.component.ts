0afec7088d3fe21f77599cb616358ea7009854b2
Wassim CHEGHAM <github@wassimchegham.com>
2017-08-15 13:11:06 +0200

    fix: tslint

--- src/app/_shared/carousel/carousel/carousel.component.ts
+++ src/app/_shared/carousel/carousel/carousel.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-carousel',
+  selector: "app-carousel",
   template: `
     <ng-content></ng-content>
   `,
