4a9c7c206a3ec7f0cf8641876bfd1ad30c153e1c
Wassim CHEGHAM <github@wassimchegham.com>
2017-08-15 13:07:55 +0200

    chore: add github scopes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,11 +13,25 @@ export const environment = {
     storageBucket: "project-2966895523221308821.appspot.com",
     messagingSenderId: "571506926720"
   },
-  scopes: [
-    {
-      uri: "https://www.googleapis.com/auth/cloud-platform",
-      description:
-        "View and manage your data across Google Cloud Platform services"
-    }
-  ]
+  scopes: {
+    google: [
+      {
+        name: "https://www.googleapis.com/auth/cloud-platform",
+        description:
+          "View and manage your data across Google Cloud Platform services"
+      }
+    ],
+    github: [
+      {
+        name: "user",
+        description:
+          "Grants read/write access to profile info only. Note that this scope includes user:email and user:follow."
+      },
+      {
+        name: "repo",
+        description:
+          "Grants read/write access to code, commit statuses, invitations, collaborators, adding team memberships, and deployment statuses for public and private repositories and organizations."
+      }
+    ]
+  }
 };
