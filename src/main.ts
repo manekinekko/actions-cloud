a56460a7d7852a1019863b2d6f062ba7c9331628
Wassim CHEGHAM <github@wassimchegham.com>
2017-08-15 13:07:36 +0200

    chore: remove firebase persistence

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+
+import * as firebase from "firebase/app";
 
 if (environment.production) {
   enableProdMode();
@@ -10,6 +12,7 @@ if (environment.production) {
 
 (async() => {
   await platformBrowserDynamic().bootstrapModule(AppModule);
+
   if ("serviceWorker" in navigator) {
     try {
       await navigator.serviceWorker.register("/assets/sw.js");
@@ -17,4 +20,22 @@ if (environment.production) {
       console.warn("cannot register ServiceWorker");
     }
   }
+
+  // make sure to call this AFTER angular bootstraps!
+  // tell Firebase not to store the auth info, we'll take care of that.
+  await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+  
 })();
+
+
+
+// const oReq = new XMLHttpRequest();
+// oReq.open("GET", "https://firebasestorage.googleapis.com/v0/b/project-2966895523221308821.appspot.com/o/actions-on-google-project-template-master.zip?alt=media&token=dea4920d-9369-4ba4-879c-1a17a66c9d31", true);
+// oReq.responseType = "blob";
+
+// oReq.onload = function(oEvent) {
+//   const blob = oReq.response;
+//   console.log(blob);
+// };
+
+// oReq.send();
