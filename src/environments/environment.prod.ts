939420f9ee128751e0de4553862efa610facc151
Wassim CHEGHAM <github@wassimchegham.com>
2017-09-16 12:19:40 +0200

    chore: change firebase project config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAwMND1TJ-dzM4dtEpaplGiSvTEaojxeFE",
-    authDomain: "project-2966895523221308821.firebaseapp.com",
-    databaseURL: "https://project-2966895523221308821.firebaseio.com",
-    projectId: "project-2966895523221308821",
-    storageBucket: "project-2966895523221308821.appspot.com",
-    messagingSenderId: "571506926720"
+    apiKey: "AIzaSyAxLYxpqFK8HC1M9_PwaW9hXKz51xJI1E0",
+    authDomain: "actions-cloud.firebaseapp.com",
+    databaseURL: "https://actions-cloud.firebaseio.com",
+    projectId: "actions-cloud",
+    storageBucket: "actions-cloud.appspot.com",
+    messagingSenderId: "498726315028"
   },
   scopes: {
     google: [
